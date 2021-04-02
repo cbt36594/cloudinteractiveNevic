7053d1cd104178fdbaf5ffa7ddb2d67f25bc54c2
Nevic <nevic0926@gmail.com>
2021-04-02 15:09:08 +0800

    add timber and lru cache

--- app/src/main/java/com/example/cloudinteractivenevic/apiresult/ApiResultBinding.kt
+++ app/src/main/java/com/example/cloudinteractivenevic/apiresult/ApiResultBinding.kt
@@ -2,9 +2,11 @@ package com.example.cloudinteractivenevic.apiresult
 
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
+import android.util.Log
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.cloudinteractivenevic.extension.BitmapCache
 import com.example.cloudinteractivenevic.extension.toBitmap
 import com.example.cloudinteractivenevic.model.Photos
 import kotlinx.coroutines.*
@@ -33,18 +35,25 @@ object ApiResultBinding {
     ) {
         val connection: URLConnection = URL(url).openConnection()
         connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit / 537.36(KHTML, like Gecko) Chrome  47.0.2526.106 Safari / 537.36")
+        val bitmapCache = BitmapCache()
+        if(bitmapCache.getBitmap(url) == null) {
 
-        val result: Deferred<Bitmap?> = GlobalScope.async(Dispatchers.IO) {
-            connection.toBitmap()
-        }
-        GlobalScope.launch(Dispatchers.Main) {
-            val bitmap = result.await()
-
-            if (bitmap != null) {
-                image.setImageBitmap(bitmap)
-            } else {
-                image.setImageDrawable(placeHolder)
+            val result: Deferred<Bitmap?> = GlobalScope.async(Dispatchers.IO) {
+                connection.toBitmap()
             }
+            GlobalScope.launch(Dispatchers.Main) {
+
+                val bitmap = result.await()
+
+                if (bitmap != null) {
+                    bitmapCache.putBitmap(url, bitmap)
+                    image.setImageBitmap(bitmapCache.getBitmap(url))
+                } else {
+                    image.setImageDrawable(error)
+                }
+            }
+        } else {
+            image.setImageBitmap(bitmapCache.getBitmap(url))
         }
 
 
--- app/src/main/java/com/example/cloudinteractivenevic/extension/BitmapCache.kt
+++ app/src/main/java/com/example/cloudinteractivenevic/extension/BitmapCache.kt
@@ -1,25 +1,31 @@
 package com.example.cloudinteractivenevic.extension
 
 import android.graphics.Bitmap
-import android.util.Log
 import android.util.LruCache
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 
 class BitmapCache(maxSize: Int = (Runtime.getRuntime().maxMemory() / 1024).toInt() / 8) : LruCache<String, Bitmap>(
     maxSize
-) , ImageCache{
+) , ImageCache {
+
+    init {
+        Timber.plant(DebugTree())
+        Timber.tag("TestBit")
+    }
 
     override fun sizeOf(key: String?, value: Bitmap?): Int {
         return if (value == null) 0 else value.rowBytes * value.height / 1024
     }
 
     override fun getBitmap(url: String?): Bitmap? { //透過 url 檢查有沒有圖在 cache 中，有就回傳。或可不可以新建，可以就回傳。
-        Log.d("TestBit: ", "getlruCache ${get(url)}")
+        Timber.d("TestBit: getlruCache ${get(url)}")
         return get(url)
     }
 
     override fun putBitmap(url: String?, bitmap: Bitmap?) { //把圖存到 cache 中
-        Log.d("TestBit: ", "putlruCache")
+        Timber.d("TestBit: putlruCache")
         put(url, bitmap)
     }
 
