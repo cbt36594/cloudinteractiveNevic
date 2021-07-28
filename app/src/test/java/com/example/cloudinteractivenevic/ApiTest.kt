1e9a47664669aa3df68a4aa94f4d2dd9fabf0a90
TLC\Z00048059 <nevic.li@taiwanlottery.com.tw>
2021-07-28 15:44:52 +0800

    修正Test API寫法

--- app/src/test/java/com/example/cloudinteractivenevic/ApiTest.kt
+++ app/src/test/java/com/example/cloudinteractivenevic/ApiTest.kt
@@ -1,15 +1,20 @@
 package com.example.cloudinteractivenevic
 
 import android.util.Log
+import androidx.lifecycle.viewModelScope
 import com.example.cloudinteractivenevic.api.CloudService
 import com.example.cloudinteractivenevic.api.adpter.FlowCallAdapterFactory
 import com.example.cloudinteractivenevic.api.converter.defaultGsonConverterFactory
 import com.example.cloudinteractivenevic.api.httpClient.defaultClientBuilder
+import com.example.cloudinteractivenevic.extension.launchInWithDefaultErrorHandler
 import com.example.cloudinteractivenevic.extension.onApiFailed
 import com.example.cloudinteractivenevic.model.Photos
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
+import okhttp3.Protocol
+import okhttp3.internal.immutableListOf
 import org.junit.Test
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -18,7 +23,8 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 class ApiTest {
     val client: OkHttpClient
         get() {
-            return defaultClientBuilder
+            return defaultClientBuilder()
+                .protocols(immutableListOf(Protocol.HTTP_1_1))
                 .build()
         }
 
@@ -34,38 +40,38 @@ class ApiTest {
     @Test
     fun testApi() {
         var temp : List<Photos>
-        runBlocking {
-            api.fetchPhotos()
-                .onStart {
-
-                }
-                .catch { error ->
-                    println(error.message)
-                }
-                .onCompletion {
-
-                }
-                .collectLatest {
-                    temp = it
-                    println(it)
-                }
-        }
+//        runBlocking {
+//            api.fetchPhotos()
+//                .onStart {
+//
+//                }
+//                .catch { error ->
+//                    println(error.message)
+//                }
+//                .onCompletion {
+//
+//                }
+//                .collectLatest {
+//                    temp = it
+//                    println(it)
+//                }
+//        }
     }
 
     @Test
     fun testApi2() {
 
         runBlocking {
-//            api.fetchPhotos()
-//                .onApiFailed {
-//                    Log.d("nevic", "${it.error.detail}")
-//                    println(it)
-//                }.map { it.body() }
-//                .filterNotNull()
-//                .onEach {
-//                    Log.d("nevic", "${it.id}")
-//                    println(it)
-//                }
+            api.fetchPhotos()
+                .onApiFailed {
+                    Log.d("nevic", "${it.error.detail}")
+                    println(it)
+                }.map { it.body() }
+                .filterNotNull()
+                .collectLatest {
+                    Log.d("nevic", "${it[0].id}")
+                    println(it[0].id)
+                }
         }
     }
 }
